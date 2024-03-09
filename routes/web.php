b1c931bc043bf18b79dfdd61da6c08a7cc1a9b23
Yakup Sarı <61888196+cekapykp@users.noreply.github.com>
2024-03-09 19:21:44 +0300

    feat : resource controller and route model binding
    - php artisan make:controller --resource ListingController

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
-
+use App\Http\Controllers\ListingController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,6 @@ use App\Http\Controllers\IndexController;
 
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
+
+Route::resource('listing', ListingController::class)
+    ->only(['index', 'show']);
