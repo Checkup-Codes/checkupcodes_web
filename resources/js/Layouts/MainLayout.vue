349f28b6e91a172df4d9086a768bb246c45a9636
Yakup Sarı <61888196+cekapykp@users.noreply.github.com>
2024-03-10 00:09:22 +0300

    feat : computed data and conditional rendering

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -2,13 +2,33 @@
   <Link href="/">Main Page</Link>&nbsp;
   <Link href="/hello">Show Page</Link>
   <div>The page with time {{ timer }}</div>
+  
+  <div v-if="flashSuccess" class="success">
+    {{ flashSuccess }}
+  </div>
+  <div>{{ y }}</div>
+
   <slot>Default</slot>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/inertia-vue3'
-import { ref } from 'vue'
+import { computed,ref } from 'vue'
+import { Link, usePage } from '@inertiajs/inertia-vue3'
+
+
+const page = usePage()
+const flashSuccess = computed(
+  () => page.props.value.flash.success,
+)
 
 const timer = ref(0)
 setInterval(() => timer.value++, 1000)
 </script>
+
+
+<style scoped>
+  .success {
+    background-color: green;
+    color: white;
+  }
+</style>
