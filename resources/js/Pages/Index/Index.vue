31619db3b1c885dd312ef30b54c8d53173608640
Yakup Sarı <61888196+cekapykp@users.noreply.github.com>
2024-03-09 13:59:43 +0300

    feat : linter (eslint) - jsconfig and setting vscode
    - npm i --save-dev eslint eslint-plugin-vue

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,11 +1,11 @@
 <template>
-    <div>Index</div>
-    <Link href="/hello">Show Page</Link>
-    <div>The message is {{ message }}</div>
+  <div>Index</div>
+  <Link href="/hello">Show Page</Link>
+  <div>The message is {{ message }}</div>
 </template>
 
 <script setup>
-import { Link } from "@inertiajs/inertia-vue3";
+import { Link } from '@inertiajs/inertia-vue3'
 
-defineProps({ message: String });
+defineProps({ message: String })
 </script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
-import MainLayout from './Layouts/MainLayout.vue'
+import MainLayout from '@/Layouts/MainLayout.vue'
 
 createInertiaApp({
   resolve: async (name) => {
@@ -10,7 +10,7 @@ createInertiaApp({
     page.default.layout = page.default.layout || MainLayout
 
     return page 
-   },
+  },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
