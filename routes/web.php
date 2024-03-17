45fe9d6bfa97bd950996d8cc66561567a8c27c8f
Yakup Sarı <61888196+cekapykp@users.noreply.github.com>
2024-03-17 12:21:40 +0300

    feat : auth -registration forms
     - php artisan make:controller UserAccountController

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserAccountController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -32,3 +34,6 @@ Route::post('login', [AuthController::class, 'store'])
     ->name('login.store');
 Route::delete('logout', [AuthController::class, 'destroy'])
     ->name('logout');
+
+Route::resource('user-account', UserAccountController::class)
+    ->only(['create']);
